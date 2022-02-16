2fbc1564c98bef0c3176b0625b25d99a91a7f9e7
AleexCaires <tiagoaleex98@gmail.com>
2022-02-16 14:35:23 +0000

    Created Folders for Components

---
+++ components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Nav, NavContainer, NavLogo} from './NavbarElements'
+
+
+const Navbar = () => {
+  return (
+    <>
+        <Nav>
+          <NavContainer>
+            <NavLogo>Alex</NavLogo>
+          </NavContainer>
+        </Nav>
+    </>
+  )
+}
+
+export default Navbar
