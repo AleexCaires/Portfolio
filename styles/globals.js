dd17774ef10fb5abcbb118c109dad744cf8aa5e8
AleexCaires <tiagoaleex98@gmail.com>
2022-02-17 14:09:51 +0000

    added global Styles

---
+++ styles/globals.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    background-color: #251E3E;
+    font-family: 'Roboto', sans-serif;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`;
