0d7a9c21be1fc8d827f540f9e485d0a4cbb30607
AleexCaires <tiagoaleex98@gmail.com>
2022-02-17 14:09:38 +0000

    Created Files for Breakpoints and theme, deleted old css

---
+++ theme/theme.js
@@ -0,0 +1,13 @@
+import {breakpoints} from './breakpoints';
+
+export const theme = {
+    color: {
+        mainBG: '#251E3E',
+        secondaryBG: '#2B2348',
+        primaryColor: '#FF5252',
+        secondaryColor: '#851E3E',
+    },
+    breakpoints: {
+        breakpoints,
+    }
+}
