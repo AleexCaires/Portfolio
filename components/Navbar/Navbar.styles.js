cd997dd06e10a0bac8252fba6c451278687e5792
AleexCaires <tiagoaleex98@gmail.com>
2022-02-18 01:21:52 +0000

    Added navbar Styles

--- components/Navbar/Navbar.styles.js
+++ components/Navbar/Navbar.styles.js
@@ -4,56 +4,81 @@ import LogoIcon from '../Logo/LogoIcon';
 
 export const Nav = styled.nav`
     background: rgba(0, 0 , 0 , 0.1);
+    height: 40px;
+
+    @media (min-width: 768px) {
+    height: 60px;
+  }
+  @media (min-width: 1024px) {
     height: 80px;
+  }
 `
 export const NavbarContainer = styled.div`
     display: flex;
     flex-wrap: nowrap;
     flex-direction: row;
     justify-content: space-between;
-    height: 80px;
+    align-items: center;
+    height: 40px;
     z-index: 1;
     padding: 0 24px;
+
+    @media (min-width: 768px) {
+    height: 60px;
+  }
+  @media (min-width: 1024px) {
+    height: 80px;
+  }
 `
 export const Icon = styled(LogoIcon)`
     cursor: pointer;
+    width: 30px;
+    height: 30px;
 `
 
 export const IconWrapper = styled.div`
     justify-self: flex-start;
     cursor: pointer;
     display: flex;
-    align-items: center;
-
 `
 
 export const NavMenu = styled.ul`
-    display:  flex;
-    justify-content: center;
-    list-style: none;
-    text-align: center;
+
+    @media (min-width: 768px) {
+        display:  flex;
+        justify-content: center;
+        list-style: none;
+        text-align: center;
+    }
+    
+    @media (min-width: 1024px) {
+        display:  flex;
+        justify-content: center;
+        list-style: none;
+        text-align: center;
+    }
 `
 export const NavItem = styled.li`
     height: auto;
 `
 export const NavLinks = styled.a`
-color: #fff;
-display: flex;
-align-items: center;
-text-decoration: none;
-padding: 0 1rem;
-height: 100%;
-cursor: pointer;
-&.active {
-  border-bottom: 2px solid #FF5252;
-}
-&:hover {
-    color: #FF5252;
+    color: #fff;
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    padding: 0 1.5rem;
+    height: 100%;
+    font-weight: 300;
+    &.active {
+        border-bottom: 2px solid ${p => p.theme.color.primaryColor};
+    }
+    &:hover {
+        color: ${p => p.theme.color.primaryColor};
     }
 
     span{
         margin-right: 5px;
-        color: #FF5252;
+        color: ${p => p.theme.color.primaryColor};
     }
 `
 
@@ -66,8 +91,8 @@ export const NavBtnLink = styled.a`
     background: transparent;
     white-space: nowrap;
     padding: 10px 22px;
-    color: #FF5252;
-    border: 1px solid #FF5252;
+    color: ${p => p.theme.color.primaryColor};
+    border: 1px solid ${p => p.theme.color.primaryColor};
     font-size: 16px;
     outline: none;
     cursor: pointer;
@@ -75,16 +100,18 @@ export const NavBtnLink = styled.a`
     text-decoration: none;
     text-transform: uppercase;
     text-align: center;
+    font-weight: 300;
+
     &:hover {
         transition: all 0.2s ease-in-out;
-        background: #851E3E;
+        background: ${p => p.theme.color.secondaryColor};
         color: #251E3E;
     }
+    
 `
 
 export const Hamburger = styled.figure`
     align-self: center;
-    max-height: 80px;
     cursor: pointer;
     .inner {
         width: 18px;
@@ -140,7 +167,7 @@ export const Hamburger = styled.figure`
 
 export const MobileMenu = styled.div`
     display: block;
-    height: 0;
+    height: 100%;
     width: 100%;
     transform: rotateX(180deg);
     overflow: hidden;
@@ -150,3 +177,7 @@ export const MobileMenu = styled.div`
         height: auto;
     }
 `
+
+export const MobileWrapper = styled.div`
+`
+
