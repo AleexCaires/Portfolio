39e9124f31e98376401772cfcbbfbe08e95bfd82
AleexCaires <tiagoaleex98@gmail.com>
2022-02-21 19:53:31 +0000

    Added Contact Logic & Styles

---
+++ components/Contact/Contact.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Container } from "../Container/Container";
+import { Headings } from '../Headings/Headings';
+import { Links } from '../Links/Links'
+import { ContactWrapper, Text } from './Contact.styles';
+import { Btn, BtnInfo } from '../Button/Button.styles';
+const Contact = () => {
+  return (
+    <Container id="Contact">
+        <Headings number="4" heading="Contact" position="right" />
+        <ContactWrapper>
+          <Text>
+            <p>Do you need a helping hand on your project?</p>
+            <p>If you want to get in touch to discuss an amazing project opportunity, job role, consultation, or you just want to say "Hi", feel free to contact me via using one of the options bellow.
+</p>
+            <p><span>Twitter</span><Links target="_blank" href="https://twitter.com/" rel="noopener noreferrer">AlexIgurtes</Links></p>
+            <p><span>LinkedIn</span><Links target="_blank" href="https://twitter.com/" rel="noopener noreferrer">AlexIgurtes</Links></p>
+
+          </Text>
+          <Btn>
+            <BtnInfo to='/signin'>Email me</BtnInfo>
+          </Btn>
+        </ContactWrapper>
+    </Container>
+  )
+}
+
+export default Contact
---
+++ components/Contact/Contact.styles.js
@@ -0,0 +1,48 @@
+import styled from 'styled-components'
+import { breakpoints } from '../../theme/breakpoints'
+import media from "styled-media-query"
+
+export const Title = styled.p`
+font-size: 42px;
+border-bottom: 1px solid ${p => p.theme.color.primaryColor};
+color: #fff;
+
+span{
+    color: ${p => p.theme.color.primaryColor};
+    margin-right: 10px;
+}
+`
+
+export const ContactWrapper = styled.div`
+ 
+ ${media.greaterThan(breakpoints.large())`
+        text-align: center;
+  `}
+`
+export const Text = styled.div`
+     width: 100%;
+  color: #fff;
+  line-height: 1.8;
+  font-size: 16px;
+  margin-right: 30px;
+  font-weight: lighter;
+
+  ${media.greaterThan(breakpoints.large())`
+        font-size: 22px
+  `}
+
+  p:first-child {
+    font-size: 26px;
+
+    ${media.greaterThan(breakpoints.large())`
+        font-size: 26
+  `}
+  }
+  p:last-child {
+    margin-top: -15px;
+  }
+
+  span{
+      margin-right: 5px;
+  }
+`
