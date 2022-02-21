70eeae1f3461df0cc4aaf6de0c9035e78841c0e8
AleexCaires <tiagoaleex98@gmail.com>
2022-02-21 15:25:12 +0000

    Added breakpoints

--- theme/breakpoints.js
+++ theme/breakpoints.js
@@ -2,4 +2,8 @@ export const breakpoints = {
     sm: 768,
     lg: 1024,
     xl: 1440,
+    // https://github.com/morajabi/styled-media-query
+    small: function() {return this.sm+'px'},
+    large: function() {return this.lg+'px'},
+    xlarge: function() {return this.xl+'px'},
 }
