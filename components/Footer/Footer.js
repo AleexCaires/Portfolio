b879b5cb292d3f329cd87d133b88ef9f9542aa05
AleexCaires <tiagoaleex98@gmail.com>
2022-03-01 14:17:34 +0000

    Link updates

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
     <>
       <FooterWrapper>
         <FooterText>
-          <p>© {new Date().getFullYear()} - Coded by Alex Caires <br /> powered by <Links target="_blank" href="https://www.linkedin.com/in/alex-caires-b7b39b174/" rel="noopener noreferrer">NextJS</Links> and Hosted by <Links target="_blank" href="https://www.linkedin.com/in/alex-caires-b7b39b174/" rel="noopener noreferrer">Vercel.</Links></p>
+          <p>© {new Date().getFullYear()} - Coded by Alex Caires <br /> powered by <Links target="_blank" href="https://nextjs.org/" rel="noopener noreferrer">NextJS</Links> and Hosted by <Links target="_blank" href="https://vercel.com" rel="noopener noreferrer">Vercel.</Links></p>
         </FooterText>
           
       </FooterWrapper>
